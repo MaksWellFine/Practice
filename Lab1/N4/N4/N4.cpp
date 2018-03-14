83a91345615039c9fdff998f4fd8e50e60916134
Maksym Pelishko <maksym.pelishko@gmail.com>
2018-03-14 14:21:20 +0200

    N4

--- Lab1/N4/N4/N4.cpp
+++ Lab1/N4/N4/N4.cpp
@@ -2,10 +2,14 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
-
+using namespace std;
 int main()
 {
+	setlocale(0,"UKR");
+	cout << "Радій життю - воно прекрасне\nДля цього маєш кожен день\nДивись на сонечко це ясне\nІ знай веселих лиш пісень" << endl;
+	system("pause");
     return 0;
 }
 
